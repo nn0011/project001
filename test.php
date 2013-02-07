620abe62eece06ba214efbce790fa43845fec871
Noel Gregor O. Ilaco <noel.ilaco@gmail.com>
2013-02-07 08:50:54 +0800

    branch 001

--- test.php
+++ test.php
@@ -6,5 +6,12 @@ function test(){
   echo "hello world";
 }
 
+function test003(){
+	echo 'This is Branch 0';
+	echo 'This is Branch 1';
+	echo 'This is Branch 2';
+	echo 'This is Branch 3';
+}
+
 
 ?>
